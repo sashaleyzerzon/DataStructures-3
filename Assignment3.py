1b5b26788737c9c29d2a8541eb783f77f0cb35b8
sashaleyzerzon <sashaleyzerzon@users.noreply.github.com>
2020-09-30 22:22:16 +0300

    Pushing and hoping it will work
    Title of your sex tape

--- Assignment3.py
+++ Assignment3.py
@@ -162,9 +162,33 @@ def BuildTree():
         T.addChild(n)
     return T
 
+def findMinforN(root: Node, N: int):
+
+    # If leaf node reached and is smaller than N
+    if root.left is None and root.right is None and root.data < N:
+        return -1
+
+    # If node's value is greater than N and left value
+    # is NULL or smaller then return the node value
+    if root.data >= N and root.left is None or root.data >= N and root.left.data < N:
+        return root.data
+
+    # if node value is smaller than N search in the
+    # right subtree
+    if root.data <= N:
+        return findMinforN(root.right, N)
+
+    # if node value is greater than N search in the
+    # left subtree
+    else:
+        return findMinforN(root.left, N)
 
 def NearestRightPoint(T, x0):
-    return None
+    n = Node(0, 0)
+
+    curr_node = T.root
+    node = findMinforN(curr_node)
+    return node
 
 
 def main():
@@ -183,8 +207,8 @@ def main():
     # end
 
     # TODO: uncomment after implementation
-    # point = NearestRightPoint(T, x0)
-    # mat[point.y, point.key] == 3
+    point = NearestRightPoint(T, x0)
+    mat[point.y, point.key] == 3
 
 
 if __name__ == '__main__':
